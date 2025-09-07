3a09d960b4baad8619ddf1c6a06b4e3d88e17139
Frank Escramosa A99 <frank.escramosa@august99.com>
2025-09-08 01:38:59 +0800

    Skills and EXP

--- src/data/skillsRings.js
+++ src/data/skillsRings.js
@@ -1,9 +1,10 @@
 export const skillsRings = [
-    { label: "WordPress",   value: 90, color: "#F2CF2D" },
-    { label: "Laravel",     value: 95, color: "#F2CF2D" },
+    { label: "WordPress",   value: 87, color: "#F2CF2D" },
+    { label: "Laravel",     value: 82, color: "#F2CF2D" },
     { label: "CodeIgniter", value: 80, color: "#F2CF2D" },
-    { label: "SSS",         value: 95, color: "#F2CF2D" },
-    { label: "CSS",         value: 95, color: "#21A1B3" },
-    { label: "JavaScript",  value: 95, color: "#21A1B3" },
-    { label: "SASS",        value: 75, color: "#C43BAD" },
+    { label: "CSS",         value: 95, color: "#F2CF2D" },
+    { label: "JavaScript",         value: 95, color: "#F2CF2D" },
+    { label: "Vue.js",  value: 50, color: "#21A1B3" },
+    { label: "React.js",  value: 40, color: "#C43BAD" },
+    { label: "Tailwind",        value: 40, color: "#C43BAD" },
 ];
--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { imagetools } from 'vite-imagetools'
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), imagetools()],
   base: '/portfolio/',   // MUST match your repository name
   build: { outDir: 'docs' } // Build output into /docs
 })
